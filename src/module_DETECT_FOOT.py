b8f832b6d6eb6450abb33b21716c18df88dfc0c8
PhucEGN <lehoangphucto@gmail.com>
2025-11-18 16:29:54 +0700

    update module_DETECT_FOOT.py - Read Config.YAML

--- src/module_DETECT_FOOT.py
+++ src/module_DETECT_FOOT.py
@@ -5,7 +5,7 @@ import win32con
 import yaml
 
 MODEL_PATH = 'src/asset/model/YOLOv11-215pic.pt'
-
+YAML_CONFIG_PATH = 'src/asset/config/config.yaml'
 class FootDetector:
     def __init__(self, model_path=MODEL_PATH):
         # Khởi tạo model YOLO
@@ -22,19 +22,51 @@ class FootDetector:
     def move_cursor(self, x, y):
         win32api.SetCursorPos((x, y))
 
+    # Read YAML config file
+    def _read_config(self, config_path):
+        """Đọc file YAML
+
+        Args:
+            config_path (str): Đường dẫn tới file cấu hình YAML.
+
+        Returns:
+            dict: Nội dung của file YAML dưới dạng dictionary.
+        """
+        with open(config_path, 'r') as file:
+            config = yaml.safe_load(file)
+        return config
+    
+    # Get Limit Box from config
+    def get_limit_box(self, config_path=YAML_CONFIG_PATH, name="Click_zone"):
+        """Get the limit box coordinates from the YAML configuration file.
+
+        Args:
+            config_path (str, optional): Path to the YAML config file. Defaults to YAML_CONFIG_PATH.
+            name (str, optional): The name of the box section in the config. Defaults to "Click_zone".
+        Returns:
+            dict: A dictionary containing the coordinates of the limit box.
+        """
+        config = self._read_config(config_path)
+        box = config.get(name, {})
+        x1, x2, y1, y2 = box.get('x1', 0), box.get('x2', 0), box.get('y1', 0), box.get('y2', 0)
+        diction = {"x1": x1, "x2": x2, "y1": y1, "y2": y2}
+        return diction
+    
 if __name__ == "__main__":
     detector = FootDetector()
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     # Giới hạn độ phân giải khung hình
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    
+
     while True:
         ret, frame = cap.read()
         if not ret:
             break
         
+        # Phát hiện điểm mốc trong khung hình
         results = detector.detect_foot(frame)
+        box = detector.get_limit_box(name="Click_zone")
         
         for result in results:
             annotated_frame = result.plot()
